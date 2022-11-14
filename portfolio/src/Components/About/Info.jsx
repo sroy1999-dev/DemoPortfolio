ce5fd5f50ebef331bf33322624802d8d18a8b4af
Suvam Roy <roysuvam1999@gmail.com>
2022-11-14 22:20:13 +0530

    new commits

---
+++ portfolio/src/Components/About/Info.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable jsx-a11y/heading-has-content */
+import React from 'react';
+
+const Info = () => {
+  return (
+    <div className='about__info grid'>
+        <div className="about__box">
+            <i className='bx bx-award about__icon'></i>
+            <h3 className="about__title">Experience</h3>
+            <span className="about__subtitle">8 years of working</span>
+        </div>
+        <div className="about__box">
+            <i className='bx bx-briefcase-alt about__icon' ></i>
+            <h3 className="about__title">Completed</h3>
+            <span className="about__subtitle">48+ Projects</span>
+        </div>
+        <div className="about__box">
+            <i className='bx bx-support about__icon'></i>
+            <h3 className="about__title">Support</h3>
+            <span className="about__subtitle">Online 24/7</span>
+        </div>
+    </div>
+  )
+}
+
+export default Info;
---
+++ portfolio/src/Components/Skills/Backend.jsx
@@ -0,0 +1,60 @@
+/* eslint-disable jsx-a11y/heading-has-content */
+import React from 'react'
+
+const Backend = () => {
+  return (
+    <div className="skills__content">
+        <h3 className="skills__title">Backend Developer</h3>
+        <div className="skills__box">
+            <div className="skills__group">
+                <div className="skills__data">
+                    <i className='bx bx-badge-check'></i>
+                    <div>
+                        <h3 className="skills__name">PHP</h3>
+                        <span className="skills__level">Intermediate</span>
+                    </div>
+                </div>
+                <div className="skills__data">
+                    <i className='bx bx-badge-check'></i>
+                    <div>
+                        <h3 className="skills__name">Node js</h3>
+                        <span className="skills__level">Basic</span>
+                    </div>
+                </div>
+                <div className="skills__data">
+                    <i className='bx bx-badge-check'></i>
+                    <div>
+                        <h3 className="skills__name">Python</h3>
+                        <span className="skills__level">Intermediate</span>
+                    </div>
+                </div>
+            </div>
+            <div className="skills__group">
+                <div className="skills__data">
+                    <i className='bx bx-badge-check'></i>
+                    <div>
+                        <h3 className="skills__name">MySQL</h3>
+                        <span className="skills__level">Intermediate</span>
+                    </div>
+                </div>
+                <div className="skills__data">
+                    <i className='bx bx-badge-check'></i>
+                    <div>
+                        <h3 className="skills__name">Firebase</h3>
+                        <span className="skills__level">Intermediate</span>
+                    </div>
+                </div>
+                <div className="skills__data">
+                    <i className='bx bx-badge-check'></i>
+                    <div>
+                        <h3 className="skills__name">Mongo DB</h3>
+                        <span className="skills__level">Intermediate</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Backend;
